7171ef9fee52935bfe9f755c2412c721ecf34bc2
Sathya-kv <sathya.kvcp@gmail.com>
2023-11-02 11:00:47 +0530

    third

---
+++ src/Components/Menu.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Menu= () => {
+  const [menuItems, setMenuItems] = useState([
+    { name: 'Burger', description: 'A delicious burger with all the fixings.', price: 10.99 },
+    { name: 'Pizza', description: 'Our signature pizza with your choice of toppings.', price: 12.99 },
+    { name: 'Pasta', description: 'Homemade pasta served with your choice of sauce.', price: 11.99 },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>Restaurant Menu</h1>
+      <ul className="menu-list">
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>Price: ${item.price.toFixed(2)}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
